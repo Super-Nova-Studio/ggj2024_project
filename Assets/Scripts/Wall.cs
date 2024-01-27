e5cb1e3e7754c815f4858008856082cde5f85f46
Daffa Ilham Ramadan <daffailhamramadan127@gmail.com>
2024-01-27 12:30:12 +0700

    add: wall spawner

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class Wall : MonoBehaviour
 {
     public float moveSpeed = 5f;
